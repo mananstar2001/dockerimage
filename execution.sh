96e015d1eacc7658b1e6383b16212b9164dbc635
Sheth, Manan <manan_sheth@optum.com>
2019-04-17 16:22:53 +0530

    Updated execution.sh for loop directory.

--- execution.sh
+++ execution.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Initializing Execution"
+jsonFolder="json"
 IFS=$'\n'; set -f
-for f in $(find /tmp/templates -name '*.jsonnet'); do echo "$f"; done
+for filePath in $(find /tmp/templates -name '*.jsonnet'); do echo "$filePath"; jsonnet -J /tmp/jsonnet "$filePath"> "${filePath/templates/$jsonFolder}"; echo "done json conversion at ${filePath/templates/$jsonFolder}"; if[ -f ${filePath/templates/$jsonFolder} ]; echo "${filePath/templates/$jsonFolder} exists"; done
 unset IFS; set +f
