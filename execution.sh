892dd24ade3008d16739782125f5a41cd9427817
Sheth, Manan <manan_sheth@optum.com>
2019-04-18 09:48:31 +0530

    Corrected shell script.

--- execution.sh
+++ execution.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 echo "Initializing Execution"
-jsonFolder="json"
+json="json"
 jsonFile=""
 IFS=$'\n'; set -f
 for filePath in $(find /tmp/templates -name '*.jsonnet'); do 
 	echo "Processing $filePath"
-	jsonFile=${filePath/templates/$jsonFolder}
+	jsonFile=${${filePath/templates/$v}/jsonnet/$json}
+	echo "jsonFile $jsonFile"
 	mkdir -p "$(dirname "$jsonFile")" && touch "$jsonFile"
-	jsonnet -J /tmp/jsonnet ${filePath} > ${jsonFile}
+	jsonnet -J /tmp/jsonnet "${filePath}" > "${jsonFile}"
 	echo "done json conversion at ${jsonFile}"
-	if [ -f $jsonFile ] 
+	if [ -f "$jsonFile" ] ; then
 		echo "${filePath/templates/$jsonFolder} exists"
-	done
+	fi
+done
 unset IFS; set +f
